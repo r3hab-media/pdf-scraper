008b767914feb3ef0029405c245d6f63161ec401
Jared Newnam <jnewnam@scottsdaleaz.gov>
2025-08-20 12:07:04 -0700

    adding pwa support

--- sw.js
+++ sw.js
@@ -8,34 +8,17 @@ const STATIC_ASSETS = [
 	"https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&display=swap",
 ];
 
-self.addEventListener("install", (event) => {
-	event.waitUntil(
-		caches.open(CACHE_NAME).then((cache) => {
-			return cache.addAll(STATIC_ASSETS);
-		})
-	);
+self.addEventListener("install", (e) => {
+	e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(STATIC_ASSETS)));
 	self.skipWaiting();
 });
-
-self.addEventListener("activate", (event) => {
-	event.waitUntil(
-		caches.keys().then((cacheNames) => {
-			return Promise.all(
-				cacheNames.map((cache) => {
-					if (cache !== CACHE_NAME) {
-						return caches.delete(cache);
-					}
-				})
-			);
-		})
-	);
+self.addEventListener("activate", (e) => {
+	e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))));
 	self.clients.claim();
 });
-
 self.addEventListener("fetch", (event) => {
-	event.respondWith(
-		caches.match(event.request).then((cachedResponse) => {
-			return cachedResponse || fetch(event.request);
-		})
-	);
+	const url = new URL(event.request.url);
+	// only handle same-origin on GH Pages
+	if (url.origin !== self.location.origin) return;
+	event.respondWith(caches.match(event.request).then((r) => r || fetch(event.request)));
 });
